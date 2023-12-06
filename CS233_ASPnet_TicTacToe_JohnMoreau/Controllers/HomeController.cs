58f7abff17c006e2198d12dbf0b49ad2877e4317
John <johnmoreau19@gmail.com>
2023-12-06 01:22:53 -0800

    Notes

--- CS233_ASPnet_TicTacToe_JohnMoreau/Controllers/HomeController.cs
+++ CS233_ASPnet_TicTacToe_JohnMoreau/Controllers/HomeController.cs
@@ -25,13 +25,15 @@ namespace CS233_ASPnet_TicTacToe_JohnMoreau.Controllers
             // Could not get the binding to match to a TicTacToe object game.Board array, it would always be intitialized with a blank board,
             // or it would come through as system.string[] if using a single field.
             // The game isn't picking up all of the fields and seems to be initializing a new board array each time...
+            // Also tried with a List of strings, but end up duplicating each time the object is deserialized due to my constructor using a default board,
+            // the updated board simply gets appended upon deserialization. 
 
 
             // This works instead... passing in each field seperately
             //var game = new TicTacToe(board, currentPlayer, int.Parse(id));
 
-            // Or store the serialized (string) version as a attribute of the class and that can be bound easily
-            // Could probably have made the attribute a List of strings instead...
+            // Or store the serialized (string) version as a attribute of the class and that can be bound easily.
+            // Why can't the model simply do this step correctly when passing back the board as a List or Array of strings???
             game.Board = JsonConvert.DeserializeObject<string[]>(game.BoardString) ?? TicTacToe.InitializeBoard();
             game.Board[game.LastId] = game.CurrentPlayer;
             game.SwitchPlayer();
