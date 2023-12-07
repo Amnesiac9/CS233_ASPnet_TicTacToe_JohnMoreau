19d5611046be146ee9ad5dcaf0ad1c21a25fa4d7
John <johnmoreau19@gmail.com>
2023-12-06 23:45:14 -0800

    Working Win Check

--- CS233_ASPnet_TicTacToe_JohnMoreau/Models/TicTacToe.cs
+++ CS233_ASPnet_TicTacToe_JohnMoreau/Models/TicTacToe.cs
@@ -50,18 +50,13 @@
 
         public void CheckForWin()
         {
-            //if (CheckPattern(Board, 0, Board.Length, 3, 1)  // Check Rows
-            //    || CheckPattern(Board, 0, Board.Length, 1, 3) // Check Columns
-            //    || CheckPattern(Board, 0, Board.Length, 2, 4) // Check left to right cross
-            //    || CheckPattern(Board, 2, 6, 2, 2)) // Check right to left cross
-            //{
-            //    GameOver = true;
-            //    PushWin();
-            //};
-            if (CheckPattern(Board, 0, Board.Length, 3, 1)  // Check Rows
-                || CheckPattern(Board, 0, Board.Length, 1, 3) // Check Columns
-                || CheckPattern(Board, 0, Board.Length, 2, 4) // Check left to right cross
-                || CheckPattern(Board, 2, 6, 2, 2)) // Check right to left cross
+
+            // 9 = outer end
+
+            if (CheckPattern(Board, 0, 3, 3, 1, 3)  // Check Rows // Start at 0, increment outer loop by 3 (Check 3 at a time), then iterate inner loop by 1 (Check left to right) inner end would be start + 3, outer end would be board.length
+                || CheckPattern(Board, 0, 1, 3, 3, 3) // Check Columns // Start at 0, increment outer loop by 1 (Check top to bottom) then iterate inner loop by 3 (Checking 3 at a time), inner end would be board.length (9), outer end would be 3.
+                || CheckPattern(Board, 0, 2, 1, 4, 3) // Check left to right cross // Start at 0, outerloop only needs to run once.
+                || CheckPattern(Board, 2, 1, 6, 2, 3)) // Check right to left cross // Start at 0, outerloop only needs to run once.
             {
                 GameOver = true;
                 PushWin();
@@ -81,12 +76,14 @@
         }
 
 
-        private static bool CheckPattern(string[] board, int start, int end, int outerIncrement, int innerIncrement)
+        private static bool CheckPattern(string[] board, int startIndex, int outerIncrement, int outerRunCount, int innerIncrement, int innerRunCount)
         {
+            if(startIndex == 2)
+            {
+                var col = 0;
+            }
 
-
-            // Check Row Wins
-            for (int i = start; i < end; i += outerIncrement)
+            for (int i = startIndex; i < outerRunCount*outerIncrement; i += outerIncrement)
             {
                 var position = "";
                 var matchCount = 0;
@@ -96,7 +93,7 @@
                     return false;
                 }
 
-                for (int j = i; j < end; j += innerIncrement)
+                for (int j = i; j < i + innerRunCount*innerIncrement; j += innerIncrement)
                 {
                     if (j >= board.Length)
                     {
@@ -132,54 +129,4 @@
 
     }
 
-    //private static bool CheckPattern(string[] board, int start, int end, int outerIncrement, int innerIncrement)
-    //{
-
-
-    //    // Check Row Wins
-    //    for (int i = start; i < end; i += outerIncrement)
-    //    {
-    //        var position = "";
-    //        var matchCount = 0;
-
-    //        if (i >= board.Length)
-    //        {
-    //            return false;
-    //        }
-
-    //        for (int j = i; j < end; j += innerIncrement)
-    //        {
-    //            if (j >= board.Length)
-    //            {
-    //                return false;
-    //            }
-    //            var lastPosition = position;
-    //            position = board[j];
-    //            if (position != "X" && position != "O")
-    //            {
-    //                continue;
-    //            }
-
-    //            if (lastPosition == position)
-    //            {
-    //                matchCount++;
-    //                if (matchCount >= 2)
-    //                {
-    //                    return true;
-    //                }
-    //            }
-    //            else
-    //            {
-    //                matchCount = 0;
-    //            }
-    //        }
-
-    //    }
-
-    //    return false;
-
-    //}
-
-
-}
 }
