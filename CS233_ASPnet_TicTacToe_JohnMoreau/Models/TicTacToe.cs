b64f382ec643da5b6bcd4bb84494f0adff185158
John <johnmoreau19@gmail.com>
2023-12-07 18:49:25 -0800

    Added comments for my more complex functions

--- CS233_ASPnet_TicTacToe_JohnMoreau/Models/TicTacToe.cs
+++ CS233_ASPnet_TicTacToe_JohnMoreau/Models/TicTacToe.cs
@@ -68,6 +68,7 @@ namespace CS233_ASPnet_TicTacToe_JohnMoreau.Models
         public void CheckForWinOrTie()
         {
 
+            
             if (CheckPattern(Board, 0, 3, 3, 1, 3)  // Check Rows // Start at 0, increment outer loop by 3 (Check 3 at a time), then iterate inner loop by 1 (Check left to right) inner end would be start + 3, outer end would be board.length
                 || CheckPattern(Board, 0, 1, 3, 3, 3) // Check Columns // Start at 0, increment outer loop by 1 (Check top to bottom) then iterate inner loop by 3 (Checking 3 at a time), inner end would be board.length (9), outer end would be 3.
                 || CheckPattern(Board, 0, 2, 1, 4, 3) // Check left to right cross // Start at 0, outerloop only needs to run once.
@@ -85,10 +86,13 @@ namespace CS233_ASPnet_TicTacToe_JohnMoreau.Models
         }
 
 
+        // I know I could have hard coded each win condition, but this was fun. =)
+        // CheckPattern traverses a grid using a start index, outerIncrement (How much the outer loop should increment), outerRunCount (How many times the outer loop should run)
         private static bool CheckPattern(string[] board, int startIndex, int outerIncrement, int outerRunCount, int innerIncrement, int innerRunCount)
         {
 
-            for (int i = startIndex; i < outerRunCount*outerIncrement; i += outerIncrement)
+            // Start at our start index, increment by the outerIncrement, and loop while i is < how much we should increment * how many times.
+            for (int i = startIndex; i < outerIncrement*outerRunCount; i += outerIncrement)
             {
                 var position = "";
                 var matchCount = 0;
@@ -98,7 +102,8 @@ namespace CS233_ASPnet_TicTacToe_JohnMoreau.Models
                     return false;
                 }
 
-                for (int j = i; j < i + innerRunCount*innerIncrement; j += innerIncrement)
+                // start at the current i, increment by innerIncrement, and loop while j is < our current index + how much we should increment * how many times.
+                for (int j = i; j < i + innerIncrement*innerRunCount; j += innerIncrement)
                 {
                     if (j >= board.Length)
                     {
